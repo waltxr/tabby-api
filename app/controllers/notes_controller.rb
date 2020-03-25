9293a270c264df293a9595744efd6189f85bf758
Adam Walter <Waltxr@users.noreply.github.com>
2020-03-25 13:08:02 -0400

    Route for getting notes done.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,13 +2,25 @@ class NotesController < ApplicationController
   before_action :authorize_request
 
   def index
-    byebug
+
     @notes = @current_user.notes
     render json: @notes
   end
 
   def create
-    byebug
+    @note = @current_user.notes.create(note_params)
+    if @note.save
+      render json: @note, status: :created
+    else
+      render json: {errors: @note.errors.full_messages},
+             status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def note_params
+    params.permit(:title, :body)
   end
 
 end
